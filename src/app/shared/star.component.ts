688afa0a9190f2f8602f2420edd669ab64f69050
Boris Hernandez <boris.hceron@gmail.com>
2018-08-23 10:48:08 -0600

    Making use of nested components

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnChanges } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+// import { EventEmitter } from 'protractor';
 @Component({
     selector: 'pm-star',
     templateUrl: './star.component.html',
     styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges {
-    rating: number =4;
+    @Input() rating: number;
     starWidth: number;
+    @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter<string>();
 
     ngOnChanges(): void {
         this.starWidth = this.rating * 75 / 5;
+    }
+
+    onClick(): void {
+        //Raise the event to the container
+        this.ratingClicked.emit(`The rating ${this.rating} was clicked`);
     }
 }
