7674a8968ec8f0db05330471f7553508478067a8
Boris Hernandez <boris.hceron@gmail.com>
2018-08-27 15:50:48 -0600

    Adding routes and additional components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `<div>
-    <h1>{{pageTitle}}</h1>
-    <pm-products></pm-products>
-  </div>`
+  template: `
+      <nav class='navbar navbar-expand navbar-light bg-light'>
+        <a class='navbar-brand'>{{pageTitle}}</a>
+        <ul class='nav navbar-nav'>
+          <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+          <li><a class='nav-link' [routerLink]="['/products']">Product List</a></li>
+        </ul>
+      </nav>
+      <div class='container'>
+        <router-outlet></router-outlet>
+      </div>
+`
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Managment';
 }
-
-/*
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-
-*/
